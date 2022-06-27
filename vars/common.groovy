9de9dd56b0eea86431adfb5d799d0a808894a6ac
jennifermhaiki <105896496+jennifermhaiki@users.noreply.github.com>
2022-06-27 17:51:10 -0600

    configured Tomcat deployment

--- vars/common.groovy
+++ vars/common.groovy
@@ -2,14 +2,22 @@ def call(String stageName){
   
   if ("${stageName}" == "Build")
      {
-       sh "mvn clean package"
+       sh "echo building the application with Maven"
+       //sh "mvn clean package"
      }
   else if ("${stageName}" == "SonarQube Report")
      {
-       sh "mvn clean sonar:sonar"
+       sh "echo performing code test with sonarqube"
+       //sh "mvn clean sonar:sonar"
      }
   else if ("${stageName}" == "Upload Into Nexus")
      {
-       sh "mvn clean deploy"
+       sh "echo packing up to Nexus"
+       //sh "mvn clean deploy"
+     }
+  else if ("${StageName}" == "Deoploy to Tomcat")
+     {
+       sh "echo deploying to Tomcat"
+       deploy adapters: [tomcat9(credentialsId: '35895ddc-28e7-4a5c-8085-5fa7e6d89457', path: '', url: 'http://http://54.184.60.63:9000/')], contextPath: null, war: 'target/*.war'
      }
 }
