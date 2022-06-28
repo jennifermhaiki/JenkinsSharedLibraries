2d791dc9a59a5635d057c51e7429c79e289f83aa
jennifermhaiki <105896496+jennifermhaiki@users.noreply.github.com>
2022-06-27 18:02:34 -0600

    Update common.groovy

--- vars/common.groovy
+++ vars/common.groovy
@@ -18,6 +18,6 @@ def call(String stageName){
   else if ("${StageName}" == "Deoploy to Tomcat")
      {
        sh "echo deploying to Tomcat"
-       deploy adapters: [tomcat9(credentialsId: '35895ddc-28e7-4a5c-8085-5fa7e6d89457', path: '', url: 'http://http://54.184.60.63:9000/')], contextPath: null, war: 'target/*.war'
+       //deploy adapters: [tomcat9(credentialsId: '35895ddc-28e7-4a5c-8085-5fa7e6d89457', path: '', url: 'http://http://54.184.60.63:9000/')], contextPath: null, war: 'target/*.war'
      }
 }
